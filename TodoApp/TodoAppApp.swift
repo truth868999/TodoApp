e6377fb958d6b0e0f438d54d45b168f6b1439219
truth868969 <truth8689@gmail.com>
2022-12-13 16:52:41 +0900

    Initial Commit

---
+++ TodoApp/TodoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoAppApp.swift
+//  TodoApp
+//
+//  Created by 齊藤真 on 2022/12/13.
+//
+
+import SwiftUI
+
+@main
+struct TodoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
